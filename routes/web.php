5ec10f2afd7b47aaf31dac12e9a95982d51ed4cf
ZnKovalskia <onigen.demon@gmail.com>
2025-02-02 12:12:57 +0700

    Pisah Layouts

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/tables', function () {
+    return view('tables');
+})->name('tables');
